6f07f90eec2299c00725f8e8f01ad42135fdc54d
Jenkin Schibel <jenkin.schibel@protonmail.com>
2022-07-25 19:40:10 -0700

    changed attacker schema to use generated values

--- Backend/models/Attacker.js
+++ Backend/models/Attacker.js
@@ -1,17 +1,20 @@
-const mongoose = require("mongoose");
-const { Schema } = require("mongoose");
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
 const Attack = new Schema(
-  {
-    _id: { type: Number, required: false },
-    Name: { type: String, required: false },
-    Shots_Number: { type: Number, required: true },
-    BS: { type: Number, required: true },
-    S: { type: Number, required: true },
-    AP: { type: Number, required: true },
-    Damage: { type: Number, required: true },
-  },
-  { timestamps: true }
+    {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            auto: true,
+        },
+        Name: { type: String, required: false },
+        Shots_Number: { type: Number, required: true },
+        BS: { type: Number, required: true },
+        S: { type: Number, required: true },
+        AP: { type: Number, required: true },
+        Damage: { type: Number, required: true },
+    },
+    { timestamps: true }
 );
 
-module.exports = mongoose.model("Attacker", Attack);
+module.exports = mongoose.model('Attacker', Attack);
