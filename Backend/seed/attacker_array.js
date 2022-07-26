84a8b683aa915ffad14de4374934c0a5ff138d96
Jenkin Schibel <jenkin.schibel@protonmail.com>
2022-07-25 19:40:16 -0700

    changed attacker arraay

--- Backend/seed/attacker_array.js
+++ Backend/seed/attacker_array.js
@@ -1,28 +1,27 @@
-const db = require("../db");
-const Attack = require("../models/Attacker");
+const db = require('../db');
+const Attack = require('../models/Attacker');
 
 // Connect to the database
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const main = async () => {
-  const atck = [
-    {
-      _id: 1,
-      Name: "Space Marine Bolter",
-      Shots_Number: 2,
-      BS: 3,
-      S: 4,
-      AP: 1,
-      Damage: 1,
-    },
-  ];
+    const atck = [
+        {
+            Name: 'Space Marine Bolter',
+            Shots_Number: 2,
+            BS: 3,
+            S: 4,
+            AP: 1,
+            Damage: 1,
+        },
+    ];
 
-  await Attack.insertMany(atck);
-  console.log("Attacking foul heretics!");
+    await Attack.insertMany(atck);
+    console.log('Attacking foul heretics!');
 };
 const run = async () => {
-  await main();
-  db.close();
+    await main();
+    db.close();
 };
 
 run();
